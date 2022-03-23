31e37103844348ee4be832b7a39cd45d93db75e6
david <davidmolecula@gmail.com>
2022-03-23 11:28:47 -0300

    nuevo estilo

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const select = document.querySelector('select');
 const html = document.querySelector('html');
+const contenedora= document.querySelector('.contenedora_total')
 document.body.style.padding = '10px';
 
 function update(bgColor, textColor) {
@@ -7,6 +8,7 @@ function update(bgColor, textColor) {
   html.style.color = textColor;
   bodi.style.backgroundColor=bgColor;
   h1.style.color=textColor;
+  contenedora.style.backgroundColor=bgColor;
 }
 
 select.addEventListener('change', () => ( select.value === 'black' ) ? update('black','white') : update('white','black'));
