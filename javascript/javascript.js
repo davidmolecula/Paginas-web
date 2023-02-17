7b4b401115e754b7a3a32bede5ef6e237a98c8e6
david <davidmolecula@gmail.com>
2023-02-17 15:20:58 -0300

    vigesimo tercer commit intentando que ande el particles hay un error en la escritura de la ruta, al parecer hay que escribir ../Img/etc con los dos puntos, la barra y la I mayuscula, o eso creo

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -20,7 +20,7 @@ particlesJS("particles", {
           "nb_sides": 16
         },
         "image": {
-          "src": "Img/logobl.png",
+          "src": "../Img/logobl.png",
           "width": 100,
           "height": 100
         }
