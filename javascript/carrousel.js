d0dcdcbef47c1b18b1253454c8af168910d8a92f
david <davidmolecula@gmail.com>
2023-02-27 08:32:33 -0300

    ultimo commit en master, a partir de ahora voy a crear una rama para hacer la seccion de informacion principal y en caso de querer modificar el estilo de otras cosas voy a hacer una rama de estilo

--- javascript/carrousel.js
+++ javascript/carrousel.js
@@ -6,7 +6,7 @@ const tarjetaLink1=document.getElementById('tarjeta-link-1');
 const tarjetaLink2=document.getElementById('tarjeta-link-2');
 
 const titles=["Beca Manuel Belgrano", "Beca Bienestar","Beca Ceit","Beca progresar"];
-const texts=["Estas becas tienen como objetivo promover el ingreso, la continuidad y la graduación de estudiantes en estas disciplinas centrales para el desarrollo económico y social de nuestro país. Inscripción para los inscriptos en 2022: Desde el 1 de febrero hasta el 22 de febrero. Inscripción por primera vez: Desde el 22 de febrero hasta el 31 de marzo.", "Las becas de bienestar consisten en: Becas de servicio, Becas de ayuda economica y becas de ----", "Las becas del ceit son una ayuda que brinca el centro de estudiantes para comprar los apuntes que los estudiantes necesitan", "La beca random es la beca random"];
+const texts=[`Estas becas tienen como objetivo promover el ingreso, la continuidad y la graduación de estudiantes en estas disciplinas centrales para el desarrollo económico y social de nuestro país. Inscripción para los inscriptos en 2022: Desde el <span class="resaltar">1 de febrero hasta el 22 de febrero</span>. Inscripción por primera vez: Desde el <span class="resaltar"> 22 de febrero hasta el 31 de marzo</span>.`, "Las becas de bienestar consisten en: Becas de servicio, Becas de ayuda economica y becas de ----", "Las becas del ceit son una ayuda que brinca el centro de estudiantes para comprar los apuntes que los estudiantes necesitan", "La beca random es la beca random"];
 const tarjetaLinks2=["https://becasmanuelbelgrano.educacion.gob.ar/ingreso_postulantes.php","https://www.instagram.com/bienestar.utn/","https://www.instagram.com/ceitutn/","#"];
 const tarjetaLinks1=[];
 
