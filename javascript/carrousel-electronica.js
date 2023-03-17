dd93aa326a4f3f3b8a419fefe6cfc6e0fd201083
david <davidmolecula@gmail.com>
2023-03-17 01:33:03 -0300

    mobile

--- javascript/carrousel-electronica.js
+++ javascript/carrousel-electronica.js
@@ -2,8 +2,7 @@ const electronicaGrande =document.querySelector('.electronica-grande');
 const electronicaPunto =document.querySelectorAll('.electronica-punto');
 const electronicaTitle=document.querySelector('.electronica-info-h2');
 const electronicaText=document.querySelector('.electronica-info-text');
-
-
+const carrerasContainer=document.getElementById('carreras');
 const electronicaTitles=["Beca Manuel Belgrano", "Beca Bienestar","Beca Ceit","Beca progresar"];
 const electronicaTexts=[`Estas becas tienen como objetivo promover el ingreso, la continuidad y la graduación de estudiantes en estas disciplinas centrales para el desarrollo económico y social de nuestro país. Inscripción para los inscriptos en 2022: Desde el <span class="resaltar">1 de febrero hasta el 22 de febrero</span>. Inscripción por primera vez: Desde el <span class="resaltar"> 22 de febrero hasta el 31 de marzo</span>.`, "Las becas de bienestar consisten en: Becas de servicio, Becas de ayuda economica y becas de ----", "Las becas del ceit son una ayuda que brinca el centro de estudiantes para comprar los apuntes que los estudiantes necesitan", "La beca random es la beca random"];
 
@@ -23,41 +22,37 @@ electronicaPunto.forEach((cadaPunto, i)=> {
         electronicaPunto[i].classList.add('activo');
     })
 })
+let stringCarreras='';
 
+for(carrera of datosCarreras.carreras)
+{
+stringCarreras+=`<h1 class="titulo-planes">Planes de estudio y sus principales cambios</h1>
 
-let stringCarreras=`<h1 class="titulo-planes">Planes de estudio y sus principales cambios</h1>
-
-<div class="electronica-container">
-    <div class="electronica-carrousel">
-        <div class="electronica-grande">
-            <img src="Img/ING. ELECTRONICA/1.jpg" alt="Imagen 1" class="img">
-            <img src="Img/ING. ELECTRONICA/2.jpg" alt="Imagen 2" class="img">
-            <img src="Img/ING. ELECTRONICA/3.jpg" alt="Imagen 2" class="img">
-            <img src="Img/ING. ELECTRONICA/4.jpg" alt="Imagen 2" class="img">
-            <img src="Img/ING. ELECTRONICA/5.jpg" alt="Imagen 2" class="img">
+<div class="${carrera.carrera}-container">
+    <div class="${carrera.carrera}-carrousel">
+        <div class="${carrera.carrera}-grande">
+            <img src="Img/ing. ${carrera.carrera}/1.jpg" alt="Imagen informativa 1" class="img">
+            <img src="Img/ing. ${carrera.carrera}/2.jpg" alt="Imagen informativa 2" class="img">
+            <img src="Img/ing. ${carrera.carrera}/3.jpg" alt="Imagen informativa 3" class="img">
+            <img src="Img/ing. ${carrera.carrera}/4.jpg" alt="Imagen informativa 4" class="img">
+            <img src="Img/ing. ${carrera.carrera}/5.jpg" alt="Imagen informativa 5" class="img">
         </div>
 
-        <ul class="electronica-puntos">
-            <li class="electronica-punto activo"></li>
-            <li class="electronica-punto"></li>
-            <li class="electronica-punto"></li>
-            <li class="electronica-punto"></li>
-            <li class="electronica-punto"></li>
+        <ul class="${carrera.carrera}-puntos">
+            <li class="${carrera.carrera}-punto activo"></li>
+            <li class="${carrera.carrera}-punto"></li>
+            <li class="${carrera.carrera}-punto"></li>
+            <li class="${carrera.carrera}-punto"></li>
+            <li class="${carrera.carrera}-punto"></li>
         </ul>
     </div>
-    <div class="electronica-info">
-        <h2 class="electronica-info-h2">Becas Estratégicas Manuel Belgrano</h2>
-        <h3 class="electronica-info-h3">Convocatoria 2023</h3>
-        <p class="electronica-info-text">
-            Estas becas tienen como objetivo promover el ingreso, la continuidad y la graduación de
-            estudiantes en estas disciplinas centrales para el desarrollo económico y social de nuestro
-            país. Inscripción para los inscriptos en 2022: Desde el <span class="resaltar">1 de febrero
-                hasta el 22 de febrero</span>.
-            Inscripción por primera vez: Desde el<span class="resaltar"> 22 de febrero hasta el 31 de
-                marzo</span>.
-
+    <div class="${carrera.carrera}-info">
+        <h2 class="${carrera.carrera}-info-h2">${carrera.titulo}</h2>
+        <h3 class="${carrera.carrera}-info-h3">${carrera.subtitulo}</h3>
+        <p class="${carrera.carrera}-info-text">${carrera.descripcion}
         </p>
-    
-
     </div>
 </div>";`
+}
+console.log(stringCarreras);
+carrerasContainer.innerHTML=stringCarreras;
--- javascript/data.js
+++ javascript/data.js
@@ -59,3 +59,63 @@ let informationCards={
     ]
 };
 
+let datosCarreras={
+
+    "carreras":[
+        {
+            id:1,
+            "carrera": "electronica",
+            "imgUrl": ["Img/ING. ELECTRONICA/1.jpg","Img/ING. ELECTRONICA/2.jpgv","Img/ING. ELECTRONICA/3.jpg","Img/ING. ELECTRONICA/4.jpg","Img/ING. ELECTRONICA/5.jpg"],
+            "titulo":"Plan 2023 electronica",
+            "subtitulo":"#",
+            "descripcion":"a"
+
+        },
+        {
+            id:1,
+            "carrera": "industrial",
+            "imgUrl": ["Img/ING. INDUSTRIAL/1.jpg","Img/ING. INDUSTRIAL/2.jpgv","Img/ING. INDUSTRIAL/3.jpg","Img/ING. INDUSTRIAL/4.jpg","Img/ING. INDUSTRIAL/5.jpg","Img/ING. QUIMICA/6.jpg"],
+            "titulo":"Plan 2023 industrial",
+            "subtitulo":"#",
+            "descripcion":"a"
+
+        },
+        {
+            id:1,
+            "carrera": "civil",
+            "imgUrl": ["Img/ING. CIVIL/1.jpg","Img/ING. CIVIL/2.jpgv","Img/ING. CIVIL/3.jpg","Img/ING. CIVIL/4.jpg","Img/ING. CIVIL/5.jpg","Img/ING. CIVIL/6.jpg"],
+            "titulo":"Plan 2023 civil",
+            "subtitulo":"#",
+            "descripcion":"a"
+
+        },
+        {
+            id:1,
+            "carrera": "mecanica",
+            "imgUrl": ["Img/ING. MECANICA/1.jpg","Img/ING. MECANICA/2.jpgv","Img/ING. MECANICA/3.jpg","Img/ING. MECANICA/4.jpg","Img/ING. MECANICA/5.jpg"],
+            "titulo":"Plan 2023 mecanica",
+            "subtitulo":"#",
+            "descripcion":"a"
+
+        },
+        {
+            id:1,
+            "carrera": "electrica",
+            "imgUrl": ["Img/ING. ELECTRICA/1.jpg","Img/ING. ELECTRICA/2.jpgv","Img/ING. ELECTRICA/3.jpg","Img/ING. ELECTRICA/4.jpg","Img/ING. ELECTRICA/5.jpg"],
+            "titulo":"Plan 2023 electrica",
+            "subtitulo":"#",
+            "descripcion":"a"
+
+        },
+        {
+            id:1,
+            "carrera": "quimica",
+            "imgUrl": ["Img/ING. QUIMICA/1.jpg","Img/ING. QUIMICA/2.jpgv","Img/ING. QUIMICA/3.jpg","Img/ING. QUIMICA/4.jpg","Img/ING. QUIMICA/5.jpg"],
+            "titulo":"Plan 2023 quimica",
+            "subtitulo":"#",
+            "descripcion":"a"
+
+        }
+    ]
+};
+
